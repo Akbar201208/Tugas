9fc37ca85001df14c61bec08507c4846a2a48f5d
Akbar201208 <muhammad_firjatullah_33rpl@student.smktelkom-mlg.sch.id>
2024-08-07 14:28:20 +0700

    Add files via upload

---
+++ Siswa.java
@@ -0,0 +1,13 @@
+//object class
+public class Siswa {
+        int id;
+        String nama;
+        double ipk;
+
+//Constructor nama sama dengan nama Class
+    public Siswa() {
+      id = 0;
+      nama = "";
+      ipk = 0;
+    }
+}
